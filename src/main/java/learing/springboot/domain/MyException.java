4ab39c81f371fda8ddf63baa53d8971efda59e28
Michael Wang <qq_wp@126.com>
2017-07-24 20:53:02 +0800

    Spring Boot中Web应用的统一异常处理

---
+++ src/main/java/learing/springboot/domain/MyException.java
@@ -0,0 +1,12 @@
+package learing.springboot.domain;
+
+public class MyException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8467458989559910639L;
+
+	public MyException(String message) {
+		super(message);
+	}
+}
--- src/main/java/learing/springboot/web/HelloWebController.java
+++ src/main/java/learing/springboot/web/HelloWebController.java
@@ -6,9 +6,10 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import learing.springboot.domain.MyException;
+
 @Controller
 public class HelloWebController {
-
 
 	/**
 	 * 测试hello
@@ -20,14 +21,22 @@ public class HelloWebController {
 		model.addAttribute("name", "Dear");
 		return "/hello/someone";
 	}
-	
-	@RequestMapping("/")
-    public String index(ModelMap map) {
-        // 加入一个属性，用来在模板中读取
-        map.addAttribute("host", "http://blog.didispace.com");
-        map.addAttribute("name", "Dear");
-        // return模板文件的名称，对应src/main/resources/templates/index.html
-        return "index";  
-    }
 
+	@RequestMapping("/")
+	public String index(ModelMap map) {
+		// 加入一个属性，用来在模板中读取
+		map.addAttribute("host", "http://blog.didispace.com");
+		map.addAttribute("name", "Dear");
+		// return模板文件的名称，对应src/main/resources/templates/index.html
+		return "index";
+	}
+
+	@RequestMapping("/test_error")
+	public String testError(ModelMap map) throws Exception {
+		throw new Exception("发生一个测试错误");
+	}
+    @RequestMapping("/test_error_json")
+    public String json() throws MyException {
+        throw new MyException("发生错误2");
+    }
 }
